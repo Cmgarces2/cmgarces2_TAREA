e1048dad96c77dceaa2aea17f978ad3ac19c70c3
Cmgarces2 <dinochris@hotmail.es>
2023-02-16 13:06:02 -0500

    mover el scritp a la carpeta scripts

--- include/class.figura.php
+++ include/class.figura.php
@@ -6,6 +6,7 @@
 		
 		public static function get_form(){
 			$html = '
+			<script src="./script/Form.js"></script>
 			<form name="figuras" method="POST" action="">
 				<table border=0 align="center">
 					<tr>
@@ -56,34 +57,4 @@
 	}
 ?>
 
-<script>
-function actualizarCampos() {
-    const tipo = document.getElementsByName('tipo')[0].value;
-    const lado1 = document.getElementsByName('lado_1')[0];
-    const lado2 = document.getElementsByName('lado_2')[0];
-    const lado3 = document.getElementsByName('lado_3')[0];
 
-    switch (tipo) {
-        case 'cuadrado':
-            lado1.disabled = false;
-            lado2.disabled = true;
-            lado3.disabled = true;
-            break;
-        case 'rectangulo':
-            lado1.disabled = false;
-            lado2.disabled = false;
-            lado3.disabled = true;
-            break;
-        case 'triangulo':
-            lado1.disabled = false;
-            lado2.disabled = false;
-            lado3.disabled = false;
-            break;
-        default:
-            lado1.disabled = true;
-            lado2.disabled = true;
-            lado3.disabled = true;
-            break;
-    }
-}
-</script>
